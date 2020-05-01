c31e16e5de645b65a60e4d6dcee8b8577dde5e6f
Vlad264 <vlad-498@yandex.ru>
2020-05-01 18:42:31 +0700

    Update .gitignore

---
+++ su.nsk.iae.post.ide/src-gen/su/nsk/iae/post/ide/contentassist/antlr/internal/InternalPoSTLexer.java
@@ -0,0 +1,623 @@
+package su.nsk.iae.post.ide.contentassist.antlr.internal;
+
+// Hack: Use our own Lexer superclass by means of import. 
+// Currently there is no other way to specify the superclass for the lexer.
+import org.eclipse.xtext.ide.editor.contentassist.antlr.internal.Lexer;
+
+
+import org.antlr.runtime.*;
+import java.util.Stack;
+import java.util.List;
+import java.util.ArrayList;
+
+@SuppressWarnings("all")
+public class InternalPoSTLexer extends Lexer {
+    public static final int RULE_ID=4;
+    public static final int RULE_WS=12;
+    public static final int RULE_DIGIT=6;
+    public static final int RULE_ANY_OTHER=13;
+    public static final int RULE_SL_COMMENT=11;
+    public static final int RULE_LETTER=5;
+    public static final int RULE_BIT=8;
+    public static final int T__15=15;
+    public static final int RULE_ML_COMMENT=10;
+    public static final int RULE_HEX_DIGIT=9;
+    public static final int T__14=14;
+    public static final int EOF=-1;
+    public static final int RULE_OCTAL_DIGIT=7;
+
+    // delegates
+    // delegators
+
+    public InternalPoSTLexer() {;} 
+    public InternalPoSTLexer(CharStream input) {
+        this(input, new RecognizerSharedState());
+    }
+    public InternalPoSTLexer(CharStream input, RecognizerSharedState state) {
+        super(input,state);
+
+    }
+    public String getGrammarFileName() { return "InternalPoST.g"; }
+
+    // $ANTLR start "T__14"
+    public final void mT__14() throws RecognitionException {
+        try {
+            int _type = T__14;
+            int _channel = DEFAULT_TOKEN_CHANNEL;
+            // InternalPoST.g:11:7: ( 'Hello' )
+            // InternalPoST.g:11:9: 'Hello'
+            {
+            match("Hello"); 
+
+
+            }
+
+            state.type = _type;
+            state.channel = _channel;
+        }
+        finally {
+        }
+    }
+    // $ANTLR end "T__14"
+
+    // $ANTLR start "T__15"
+    public final void mT__15() throws RecognitionException {
+        try {
+            int _type = T__15;
+            int _channel = DEFAULT_TOKEN_CHANNEL;
+            // InternalPoST.g:12:7: ( '!' )
+            // InternalPoST.g:12:9: '!'
+            {
+            match('!'); 
+
+            }
+
+            state.type = _type;
+            state.channel = _channel;
+        }
+        finally {
+        }
+    }
+    // $ANTLR end "T__15"
+
+    // $ANTLR start "RULE_LETTER"
+    public final void mRULE_LETTER() throws RecognitionException {
+        try {
+            // InternalPoST.g:213:22: ( ( 'A' .. 'Z' | 'a' .. 'z' | '_' ) )
+            // InternalPoST.g:213:24: ( 'A' .. 'Z' | 'a' .. 'z' | '_' )
+            {
+            if ( (input.LA(1)>='A' && input.LA(1)<='Z')||input.LA(1)=='_'||(input.LA(1)>='a' && input.LA(1)<='z') ) {
+                input.consume();
+
+            }
+            else {
+                MismatchedSetException mse = new MismatchedSetException(null,input);
+                recover(mse);
+                throw mse;}
+
+
+            }
+
+        }
+        finally {
+        }
+    }
+    // $ANTLR end "RULE_LETTER"
+
+    // $ANTLR start "RULE_DIGIT"
+    public final void mRULE_DIGIT() throws RecognitionException {
+        try {
+            // InternalPoST.g:215:21: ( '0' .. '9' )
+            // InternalPoST.g:215:23: '0' .. '9'
+            {
+            matchRange('0','9'); 
+
+            }
+
+        }
+        finally {
+        }
+    }
+    // $ANTLR end "RULE_DIGIT"
+
+    // $ANTLR start "RULE_OCTAL_DIGIT"
+    public final void mRULE_OCTAL_DIGIT() throws RecognitionException {
+        try {
+            // InternalPoST.g:217:27: ( '0' .. '7' )
+            // InternalPoST.g:217:29: '0' .. '7'
+            {
+            matchRange('0','7'); 
+
+            }
+
+        }
+        finally {
+        }
+    }
+    // $ANTLR end "RULE_OCTAL_DIGIT"
+
+    // $ANTLR start "RULE_BIT"
+    public final void mRULE_BIT() throws RecognitionException {
+        try {
+            // InternalPoST.g:219:19: ( ( '0' | '1' ) )
+            // InternalPoST.g:219:21: ( '0' | '1' )
+            {
+            if ( (input.LA(1)>='0' && input.LA(1)<='1') ) {
+                input.consume();
+
+            }
+            else {
+                MismatchedSetException mse = new MismatchedSetException(null,input);
+                recover(mse);
+                throw mse;}
+
+
+            }
+
+        }
+        finally {
+        }
+    }
+    // $ANTLR end "RULE_BIT"
+
+    // $ANTLR start "RULE_HEX_DIGIT"
+    public final void mRULE_HEX_DIGIT() throws RecognitionException {
+        try {
+            // InternalPoST.g:221:25: ( ( RULE_DIGIT | 'A' .. 'F' ) )
+            // InternalPoST.g:221:27: ( RULE_DIGIT | 'A' .. 'F' )
+            {
+            if ( (input.LA(1)>='0' && input.LA(1)<='9')||(input.LA(1)>='A' && input.LA(1)<='F') ) {
+                input.consume();
+
+            }
+            else {
+                MismatchedSetException mse = new MismatchedSetException(null,input);
+                recover(mse);
+                throw mse;}
+
+
+            }
+
+        }
+        finally {
+        }
+    }
+    // $ANTLR end "RULE_HEX_DIGIT"
+
+    // $ANTLR start "RULE_ID"
+    public final void mRULE_ID() throws RecognitionException {
+        try {
+            int _type = RULE_ID;
+            int _channel = DEFAULT_TOKEN_CHANNEL;
+            // InternalPoST.g:223:9: ( RULE_LETTER ( RULE_LETTER | RULE_DIGIT )* )
+            // InternalPoST.g:223:11: RULE_LETTER ( RULE_LETTER | RULE_DIGIT )*
+            {
+            mRULE_LETTER(); 
+            // InternalPoST.g:223:23: ( RULE_LETTER | RULE_DIGIT )*
+            loop1:
+            do {
+                int alt1=2;
+                int LA1_0 = input.LA(1);
+
+                if ( ((LA1_0>='0' && LA1_0<='9')||(LA1_0>='A' && LA1_0<='Z')||LA1_0=='_'||(LA1_0>='a' && LA1_0<='z')) ) {
+                    alt1=1;
+                }
+
+
+                switch (alt1) {
+            	case 1 :
+            	    // InternalPoST.g:
+            	    {
+            	    if ( (input.LA(1)>='0' && input.LA(1)<='9')||(input.LA(1)>='A' && input.LA(1)<='Z')||input.LA(1)=='_'||(input.LA(1)>='a' && input.LA(1)<='z') ) {
+            	        input.consume();
+
+            	    }
+            	    else {
+            	        MismatchedSetException mse = new MismatchedSetException(null,input);
+            	        recover(mse);
+            	        throw mse;}
+
+
+            	    }
+            	    break;
+
+            	default :
+            	    break loop1;
+                }
+            } while (true);
+
+
+            }
+
+            state.type = _type;
+            state.channel = _channel;
+        }
+        finally {
+        }
+    }
+    // $ANTLR end "RULE_ID"
+
+    // $ANTLR start "RULE_ML_COMMENT"
+    public final void mRULE_ML_COMMENT() throws RecognitionException {
+        try {
+            int _type = RULE_ML_COMMENT;
+            int _channel = DEFAULT_TOKEN_CHANNEL;
+            // InternalPoST.g:225:17: ( '/*' ( options {greedy=false; } : . )* '*/' )
+            // InternalPoST.g:225:19: '/*' ( options {greedy=false; } : . )* '*/'
+            {
+            match("/*"); 
+
+            // InternalPoST.g:225:24: ( options {greedy=false; } : . )*
+            loop2:
+            do {
+                int alt2=2;
+                int LA2_0 = input.LA(1);
+
+                if ( (LA2_0=='*') ) {
+                    int LA2_1 = input.LA(2);
+
+                    if ( (LA2_1=='/') ) {
+                        alt2=2;
+                    }
+                    else if ( ((LA2_1>='\u0000' && LA2_1<='.')||(LA2_1>='0' && LA2_1<='\uFFFF')) ) {
+                        alt2=1;
+                    }
+
+
+                }
+                else if ( ((LA2_0>='\u0000' && LA2_0<=')')||(LA2_0>='+' && LA2_0<='\uFFFF')) ) {
+                    alt2=1;
+                }
+
+
+                switch (alt2) {
+            	case 1 :
+            	    // InternalPoST.g:225:52: .
+            	    {
+            	    matchAny(); 
+
+            	    }
+            	    break;
+
+            	default :
+            	    break loop2;
+                }
+            } while (true);
+
+            match("*/"); 
+
+
+            }
+
+            state.type = _type;
+            state.channel = _channel;
+        }
+        finally {
+        }
+    }
+    // $ANTLR end "RULE_ML_COMMENT"
+
+    // $ANTLR start "RULE_SL_COMMENT"
+    public final void mRULE_SL_COMMENT() throws RecognitionException {
+        try {
+            int _type = RULE_SL_COMMENT;
+            int _channel = DEFAULT_TOKEN_CHANNEL;
+            // InternalPoST.g:227:17: ( '//' (~ ( ( '\\n' | '\\r' ) ) )* ( ( '\\r' )? '\\n' )? )
+            // InternalPoST.g:227:19: '//' (~ ( ( '\\n' | '\\r' ) ) )* ( ( '\\r' )? '\\n' )?
+            {
+            match("//"); 
+
+            // InternalPoST.g:227:24: (~ ( ( '\\n' | '\\r' ) ) )*
+            loop3:
+            do {
+                int alt3=2;
+                int LA3_0 = input.LA(1);
+
+                if ( ((LA3_0>='\u0000' && LA3_0<='\t')||(LA3_0>='\u000B' && LA3_0<='\f')||(LA3_0>='\u000E' && LA3_0<='\uFFFF')) ) {
+                    alt3=1;
+                }
+
+
+                switch (alt3) {
+            	case 1 :
+            	    // InternalPoST.g:227:24: ~ ( ( '\\n' | '\\r' ) )
+            	    {
+            	    if ( (input.LA(1)>='\u0000' && input.LA(1)<='\t')||(input.LA(1)>='\u000B' && input.LA(1)<='\f')||(input.LA(1)>='\u000E' && input.LA(1)<='\uFFFF') ) {
+            	        input.consume();
+
+            	    }
+            	    else {
+            	        MismatchedSetException mse = new MismatchedSetException(null,input);
+            	        recover(mse);
+            	        throw mse;}
+
+
+            	    }
+            	    break;
+
+            	default :
+            	    break loop3;
+                }
+            } while (true);
+
+            // InternalPoST.g:227:40: ( ( '\\r' )? '\\n' )?
+            int alt5=2;
+            int LA5_0 = input.LA(1);
+
+            if ( (LA5_0=='\n'||LA5_0=='\r') ) {
+                alt5=1;
+            }
+            switch (alt5) {
+                case 1 :
+                    // InternalPoST.g:227:41: ( '\\r' )? '\\n'
+                    {
+                    // InternalPoST.g:227:41: ( '\\r' )?
+                    int alt4=2;
+                    int LA4_0 = input.LA(1);
+
+                    if ( (LA4_0=='\r') ) {
+                        alt4=1;
+                    }
+                    switch (alt4) {
+                        case 1 :
+                            // InternalPoST.g:227:41: '\\r'
+                            {
+                            match('\r'); 
+
+                            }
+                            break;
+
+                    }
+
+                    match('\n'); 
+
+                    }
+                    break;
+
+            }
+
+
+            }
+
+            state.type = _type;
+            state.channel = _channel;
+        }
+        finally {
+        }
+    }
+    // $ANTLR end "RULE_SL_COMMENT"
+
+    // $ANTLR start "RULE_WS"
+    public final void mRULE_WS() throws RecognitionException {
+        try {
+            int _type = RULE_WS;
+            int _channel = DEFAULT_TOKEN_CHANNEL;
+            // InternalPoST.g:229:9: ( ( ' ' | '\\t' | '\\r' | '\\n' )+ )
+            // InternalPoST.g:229:11: ( ' ' | '\\t' | '\\r' | '\\n' )+
+            {
+            // InternalPoST.g:229:11: ( ' ' | '\\t' | '\\r' | '\\n' )+
+            int cnt6=0;
+            loop6:
+            do {
+                int alt6=2;
+                int LA6_0 = input.LA(1);
+
+                if ( ((LA6_0>='\t' && LA6_0<='\n')||LA6_0=='\r'||LA6_0==' ') ) {
+                    alt6=1;
+                }
+
+
+                switch (alt6) {
+            	case 1 :
+            	    // InternalPoST.g:
+            	    {
+            	    if ( (input.LA(1)>='\t' && input.LA(1)<='\n')||input.LA(1)=='\r'||input.LA(1)==' ' ) {
+            	        input.consume();
+
+            	    }
+            	    else {
+            	        MismatchedSetException mse = new MismatchedSetException(null,input);
+            	        recover(mse);
+            	        throw mse;}
+
+
+            	    }
+            	    break;
+
+            	default :
+            	    if ( cnt6 >= 1 ) break loop6;
+                        EarlyExitException eee =
+                            new EarlyExitException(6, input);
+                        throw eee;
+                }
+                cnt6++;
+            } while (true);
+
+
+            }
+
+            state.type = _type;
+            state.channel = _channel;
+        }
+        finally {
+        }
+    }
+    // $ANTLR end "RULE_WS"
+
+    // $ANTLR start "RULE_ANY_OTHER"
+    public final void mRULE_ANY_OTHER() throws RecognitionException {
+        try {
+            int _type = RULE_ANY_OTHER;
+            int _channel = DEFAULT_TOKEN_CHANNEL;
+            // InternalPoST.g:231:16: ( . )
+            // InternalPoST.g:231:18: .
+            {
+            matchAny(); 
+
+            }
+
+            state.type = _type;
+            state.channel = _channel;
+        }
+        finally {
+        }
+    }
+    // $ANTLR end "RULE_ANY_OTHER"
+
+    public void mTokens() throws RecognitionException {
+        // InternalPoST.g:1:8: ( T__14 | T__15 | RULE_ID | RULE_ML_COMMENT | RULE_SL_COMMENT | RULE_WS | RULE_ANY_OTHER )
+        int alt7=7;
+        alt7 = dfa7.predict(input);
+        switch (alt7) {
+            case 1 :
+                // InternalPoST.g:1:10: T__14
+                {
+                mT__14(); 
+
+                }
+                break;
+            case 2 :
+                // InternalPoST.g:1:16: T__15
+                {
+                mT__15(); 
+
+                }
+                break;
+            case 3 :
+                // InternalPoST.g:1:22: RULE_ID
+                {
+                mRULE_ID(); 
+
+                }
+                break;
+            case 4 :
+                // InternalPoST.g:1:30: RULE_ML_COMMENT
+                {
+                mRULE_ML_COMMENT(); 
+
+                }
+                break;
+            case 5 :
+                // InternalPoST.g:1:46: RULE_SL_COMMENT
+                {
+                mRULE_SL_COMMENT(); 
+
+                }
+                break;
+            case 6 :
+                // InternalPoST.g:1:62: RULE_WS
+                {
+                mRULE_WS(); 
+
+                }
+                break;
+            case 7 :
+                // InternalPoST.g:1:70: RULE_ANY_OTHER
+                {
+                mRULE_ANY_OTHER(); 
+
+                }
+                break;
+
+        }
+
+    }
+
+
+    protected DFA7 dfa7 = new DFA7(this);
+    static final String DFA7_eotS =
+        "\1\uffff\1\10\2\uffff\1\6\2\uffff\1\10\5\uffff\2\10\1\20\1\uffff";
+    static final String DFA7_eofS =
+        "\21\uffff";
+    static final String DFA7_minS =
+        "\1\0\1\145\2\uffff\1\52\2\uffff\1\154\5\uffff\1\154\1\157\1\60\1\uffff";
+    static final String DFA7_maxS =
+        "\1\uffff\1\145\2\uffff\1\57\2\uffff\1\154\5\uffff\1\154\1\157\1\172\1\uffff";
+    static final String DFA7_acceptS =
+        "\2\uffff\1\2\1\3\1\uffff\1\6\1\7\1\uffff\1\3\1\2\1\4\1\5\1\6\3\uffff\1\1";
+    static final String DFA7_specialS =
+        "\1\0\20\uffff}>";
+    static final String[] DFA7_transitionS = {
+            "\11\6\2\5\2\6\1\5\22\6\1\5\1\2\15\6\1\4\21\6\7\3\1\1\22\3\4\6\1\3\1\6\32\3\uff85\6",
+            "\1\7",
+            "",
+            "",
+            "\1\12\4\uffff\1\13",
+            "",
+            "",
+            "\1\15",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "\1\16",
+            "\1\17",
+            "\12\10\7\uffff\32\10\4\uffff\1\10\1\uffff\32\10",
+            ""
+    };
+
+    static final short[] DFA7_eot = DFA.unpackEncodedString(DFA7_eotS);
+    static final short[] DFA7_eof = DFA.unpackEncodedString(DFA7_eofS);
+    static final char[] DFA7_min = DFA.unpackEncodedStringToUnsignedChars(DFA7_minS);
+    static final char[] DFA7_max = DFA.unpackEncodedStringToUnsignedChars(DFA7_maxS);
+    static final short[] DFA7_accept = DFA.unpackEncodedString(DFA7_acceptS);
+    static final short[] DFA7_special = DFA.unpackEncodedString(DFA7_specialS);
+    static final short[][] DFA7_transition;
+
+    static {
+        int numStates = DFA7_transitionS.length;
+        DFA7_transition = new short[numStates][];
+        for (int i=0; i<numStates; i++) {
+            DFA7_transition[i] = DFA.unpackEncodedString(DFA7_transitionS[i]);
+        }
+    }
+
+    class DFA7 extends DFA {
+
+        public DFA7(BaseRecognizer recognizer) {
+            this.recognizer = recognizer;
+            this.decisionNumber = 7;
+            this.eot = DFA7_eot;
+            this.eof = DFA7_eof;
+            this.min = DFA7_min;
+            this.max = DFA7_max;
+            this.accept = DFA7_accept;
+            this.special = DFA7_special;
+            this.transition = DFA7_transition;
+        }
+        public String getDescription() {
+            return "1:1: Tokens : ( T__14 | T__15 | RULE_ID | RULE_ML_COMMENT | RULE_SL_COMMENT | RULE_WS | RULE_ANY_OTHER );";
+        }
+        public int specialStateTransition(int s, IntStream _input) throws NoViableAltException {
+            IntStream input = _input;
+        	int _s = s;
+            switch ( s ) {
+                    case 0 : 
+                        int LA7_0 = input.LA(1);
+
+                        s = -1;
+                        if ( (LA7_0=='H') ) {s = 1;}
+
+                        else if ( (LA7_0=='!') ) {s = 2;}
+
+                        else if ( ((LA7_0>='A' && LA7_0<='G')||(LA7_0>='I' && LA7_0<='Z')||LA7_0=='_'||(LA7_0>='a' && LA7_0<='z')) ) {s = 3;}
+
+                        else if ( (LA7_0=='/') ) {s = 4;}
+
+                        else if ( ((LA7_0>='\t' && LA7_0<='\n')||LA7_0=='\r'||LA7_0==' ') ) {s = 5;}
+
+                        else if ( ((LA7_0>='\u0000' && LA7_0<='\b')||(LA7_0>='\u000B' && LA7_0<='\f')||(LA7_0>='\u000E' && LA7_0<='\u001F')||(LA7_0>='\"' && LA7_0<='.')||(LA7_0>='0' && LA7_0<='@')||(LA7_0>='[' && LA7_0<='^')||LA7_0=='`'||(LA7_0>='{' && LA7_0<='\uFFFF')) ) {s = 6;}
+
+                        if ( s>=0 ) return s;
+                        break;
+            }
+            NoViableAltException nvae =
+                new NoViableAltException(getDescription(), 7, _s, input);
+            error(nvae);
+            throw nvae;
+        }
+    }
+ 
+
+}
